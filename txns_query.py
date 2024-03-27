d6d4f95301a1a54ca9c4c73b76ab547c912de027
Li Zhaoxian <lzx@waw.li>
2024-03-27 22:09:12 +0800

    Add Batch Job

--- txns_query.py
+++ txns_query.py
@@ -10,6 +10,8 @@ import unittest
 
 # Seconds between query made to etherscan.io
 PER_QUERY_GAP_SEC = 6
+PER_QUERY_GAP_SEC_BINANCE = 1
+MAX_RETRY = 3
 
 logger = logging.getLogger(__name__)
 
@@ -59,7 +61,7 @@ class TxnStore(object):
         self.con.commit()
 
         # etherscan query rate limiter
-        self.ts_next_query = int(time.time())
+        self.ts_next_query = None
 
         # binance spot price cache, to reduce query
         self.eth_usd_cache = {}
@@ -98,67 +100,99 @@ class TxnStore(object):
         """ Query and Store txns earlier than queue head """
         logger.info('query_at_head ...')
         blk_head, _ = self.get_db_blk_head_tail()
-        self._query_from(blk_head - 100, blk_head, test_hold=test_hold)
+        self.query_from(blk_head - 100, blk_head, test_hold=test_hold)
 
     def query_at_tail(self, test_hold=False):
         """ Query and Store txns later than queue head """
         logger.info('query_at_tail ...')
         _, blk_tail = self.get_db_blk_head_tail()
-        self._query_from(blk_tail, blk_tail + 100, test_hold=test_hold)
+        self.query_from(blk_tail, blk_tail + 100, test_hold=test_hold, max_retry=0)
 
-    def _query_from(self, blkno, before_block, test_hold=False):
+    def query_from(self, blkno, before_block, test_hold=False, check_dup=False, max_retry=MAX_RETRY):
+        """ Query and Store txns from blkno till before_block (exclusive) """
+        logger.info(f'query_from ... {blkno} {before_block}')
+
         query_from_blk = blkno
         page = 1
         txns = []
+        hash_visited = set()
+        error_cnt = 0
+
+        if self.ts_next_query is None:
+            self.ts_next_query = int(time.time())
 
         while True:
+            if error_cnt > max_retry:
+                break
+
             # Query freq guard
             ts_now = int(time.time())
             if ts_now < self.ts_next_query:
-                time.sleep(self.ts_next_query - ts_now + 1)
+                time.sleep(self.ts_next_query - ts_now)
                 continue
             self.ts_next_query += PER_QUERY_GAP_SEC
 
             url = _get_qurl_tx(sblk=query_from_blk, page=page, offset=100)
-            page += 1
 
+            logger.info(f'{time.time()} query : {url}')
             r = requests.get(url)
             data = r.json()
 
             if data['status'] != '1':
-                logger.info(f'status : {data["status"]}')
+                error_cnt += 1
+                logger.info(f'status : {data["status"]}  ERROR_CNT:{error_cnt}')
                 logger.info(f'message: {data["message"]}')
                 logger.info(f'{query_from_blk} ~ {before_block} done')
-                break
+                continue
+
+            error_cnt = 0
+            page += 1
 
             items = data['result']
             items = [x for x in items if int(x['blockNumber']) < before_block]
+            items = [x for x in items if x['hash'] not in hash_visited]
 
             if len(items) == 0:
                 logger.info(f'{query_from_blk} ~ {before_block} done')
                 break
 
-            logger.info(f"inc {len(items)} next_page {page}")
+            if page == 10:
+                query_from_blk = items[-1]['blockNumber']
+                page = 1
+
+            logger.info(f"inc {len(items)} next_page {query_from_blk}-{page}")
             txns += items
+            hash_visited = hash_visited.union([x['hash'] for x in items])
 
         if len(txns) > 0:
-            self._save(txns)
+            self._save(txns, check_dup)
 
         # when test, recreate obj will not follow the query gap, we hold before exit
         if test_hold:
             time.sleep(PER_QUERY_GAP_SEC)
 
-    def _save(self, txns):
+    def _is_hash_exists(self, vhash):
+        cur = self.con.execute(
+            f'SELECT * FROM txns WHERE hash = "{vhash}";')
+        records = cur.fetchone()
+        return records is not None
+
+    def _save(self, txns, check_dup):
         values = []
 
+        ts_next_query = int(time.time())
         for one in txns:
+            vhash = one['hash']
+            if check_dup and self._is_hash_exists(vhash):
+                continue
+
             ts = int(one['timeStamp'])
-            eth_usd_px = get_px_eth_usd(ts, self.eth_usd_cache)
+            eth_usd_px, ts_next_query = get_px_eth_usd(ts, self.eth_usd_cache, ts_next_query)
             eth_usd_px = f'{eth_usd_px:.2f}'
             sql = ','.join([
                 one['timeStamp'],
                 one['blockNumber'],
-                f"\"{one['hash']}\"",
+                f"\"{vhash}\"",
                 f"\"{one['from']}\"",
                 f"\"{one['to']}\"",
                 f"\"{float(one['value']) / (10**float(one['tokenDecimal']))}\"",
@@ -171,10 +205,12 @@ class TxnStore(object):
             sql = f'({sql})'
             values += [sql]
 
-        values = ','.join(values)
-        sql = f'INSERT INTO txns VALUES {values};'
-        self.con.execute(sql)
-        self.con.commit()
+        logger.info(f'INSERT {len(values)} RECORDS ...')
+        if len(values) > 0:
+            values = ','.join(values)
+            sql = f'INSERT INTO txns VALUES {values};'
+            self.con.execute(sql)
+            self.con.commit()
 
 
 class TestTxnStore(unittest.TestCase):
@@ -245,7 +281,7 @@ class TestTxnStore(unittest.TestCase):
         os.remove('data_test/db.db.temp')
 
 
-def get_px_eth_usd(ts, cache):
+def get_px_eth_usd(ts, cache, ts_next_query):
     """
     Query Binance API for price at ts.
 
@@ -260,12 +296,17 @@ def get_px_eth_usd(ts, cache):
     """
     ts_query = int(ts / (15 * 60)) * 15 * 60 * 1000
     if ts_query in cache:
-        return cache[ts_query]
+        return ts_next_query, cache[ts_query]
 
     ts_query_early = ts_query - (29 * 15 * 60 * 1000)
     url = 'https://api.binance.com/api/v3/klines?' + \
           f'symbol=ETHUSDT&interval=15m&startTime={ts_query_early}&limit=30'
     try:
+        ts_now = int(time.time())
+        if ts_now < ts_next_query:
+            time.sleep(ts_next_query - ts_now)
+        ts_next_query += PER_QUERY_GAP_SEC_BINANCE
+
         r = requests.get(url)
         logger.info(f'query binance {ts} {ts_query_early} ...')
         for item in r.json():
@@ -273,9 +314,9 @@ def get_px_eth_usd(ts, cache):
             v = float(item[4])
             if k not in cache:
                 cache[k] = v
-        return cache[ts_query]
+        return ts_next_query, cache[ts_query]
     except Exception as _:
-        return -1
+        return ts_next_query, -1
 
 
 def _get_base_url():
@@ -308,53 +349,94 @@ def _get_qurl_tx(
     return url
 
 
-def _get_qurl_latest_blk(blockno=18999999):
+def get_qurl_blk_direct(ts):
     url = [
         _get_base_url(),
         '?module=block',
-        '&action=getblockcountdown',
-        f'&blockno={blockno}',
+        '&action=getblocknobytime',
+        f'&timestamp={ts}',
+        '&closest=before',
         '&apikey=YourApiKeyToken',
     ]
     url = ''.join(url)
     return url
 
 
-def _query_latest_blk():
-    logger.info('query_latest_blk ...')
+def get_qurl_latest_blk_direct(ts):
+    return get_qurl_blk_direct(ts)
 
-    # block no near latest blk, unable to find a suitable query to
-    # direct get latest block no, improve if found such query.
-    blockno = 18999999
 
-    ts_next_query = int(time.time())
+def query_blkno(ts):
+    logger.info('query_blkno ...')
+
+    url = get_qurl_latest_blk_direct(ts)
+
+    error_cnt = 0
     while True:
-        ts_now = int(time.time())
-        if ts_now < ts_next_query:
-            time.sleep(ts_next_query - ts_now + 1)
-            continue
-        ts_next_query += PER_QUERY_GAP_SEC
+        try:
+            r = requests.get(url)
+            data = r.json()
 
-        url = _get_qurl_latest_blk(blockno=blockno)
-        blockno += 1_000_000
-        r = requests.get(url)
-        data = r.json()
-        if data['status'] == '1':
+            # make sure the outer query is not exceeding threshold
+            time.sleep(PER_QUERY_GAP_SEC)
+
+            logger.info(data['result'])
+
+            return int(data['result'])
+        except Exception as _:
+            error_cnt += 1
+
+        if error_cnt > 3:
+            raise Exception(f'unable to get blkno for {ts}')
+
+
+def query_blkno_latest():
+    ts = int(time.time())
+    return query_blkno(ts)
+
+
+def store_specified_ts_range(dbpath, ts0, ts1):
+    store = TxnStore(dbpath, 0)
+    blkno0 = query_blkno(ts0)
+    blkno1 = query_blkno(ts1)
+
+    blkno2 = blkno0
+    blkno3 = blkno2 + 1000
+    while True:
+        if blkno3 > blkno1:
+            blkno3 = blkno1
+
+        if blkno2 > blkno3:
             break
 
-    logger.info(data['result']['CurrentBlock'])
+        store.query_from(blkno2, blkno3, check_dup=True)
 
-    # make sure the outer query is not exceeding threshold
-    time.sleep(PER_QUERY_GAP_SEC)
+        if blkno3 == blkno1:
+            break
 
-    return int(data['result']['CurrentBlock'])
+        blkno2 = blkno3
+        blkno3 = blkno2 + 1000
+
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
 
     dbpath = sys.argv[1]
-    startup_block_no = _query_latest_blk()
+
+    if len(sys.argv) >= 4:
+        ts0 = int(sys.argv[2])
+        ts1 = int(sys.argv[3])
+
+        store_specified_ts_range(dbpath, ts0, ts1)
+
+        ts0 = pd.Timestamp(float(ts0), unit='s').strftime('%Y%m%d %H:%M:%S')
+        ts1 = pd.Timestamp(float(ts1), unit='s').strftime('%Y%m%d %H:%M:%S')
+        logger.info(f"query txns period: {ts0} ~ {ts1} UTC")
+
+        return None
+
+    startup_block_no = query_blkno_latest()
     store = TxnStore(dbpath, startup_block_no)
 
     while True:
